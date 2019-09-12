1f7716359869c3677e258b421d8031c0c7ca611a
Andrea Perez Isla <andrea.perezisla.practicas@telefonica.com>
2019-09-12 12:41:12 +0200

    Add files via upload

---
+++ correostmp.py
@@ -0,0 +1,55 @@
+import boto3
+import smtplib
+import email.utils
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import jinja2
+import os
+#Cojo los credenciales
+#Hacer funcion para coger esos parámetros
+
+@correo
+
+def mandarcorreonormal(user,password,address,cc):
+	recipients = None
+	#Cambiar valores por los mios propios
+	server = smtplib.SMTP(host='host_address', port=your_port)
+	#Cambiar el sender cuando me salgan las pruebas a no reply
+	self.sender_email='no-reply@na.telefonicadev.com'
+	self.sender_name='NA-Pruebas'
+	#user es el usuario para poder usar smtp
+	self.user=user
+	self.password=password
+
+	msg = MIMEMultipart()
+
+	message = "hola"
+
+	#Parametros
+	password = self.password
+	msg['From'] = email.utils,formataddr((self.sender_name,self.sender_email))
+	msg['To'] = ','.join(address)
+	msg['Subject'] = "Acceso a AWS Network Analytics entorno de ....."
+
+	if cc is not None:
+		msg['cc']=','.join(cc)
+		recipients = address + cc
+	else:
+		recipients = address
+
+	msg.attach(MIMEText(message,'plain','UTF-8'))
+	msg.attach(MIMEText(message,'html','UTF-8'))
+
+	try:
+		self.smtp_host = 'email-smtp.eu-west-1.amazonaws.com'
+		self.smtp_port = 587
+		server = smtplib.SMTP(self.smtp_host,self.smtp_port)
+		server.ehlo()
+		server.starttls()
+		server.ehlo()
+		server.login(self.user,self.password)
+		server.sendmail(self.sender,recipients,msg.as_string())
+		server.close()
+		print('Email Sent!')
+	except Exception as e:
+		print("Error: ",e)
