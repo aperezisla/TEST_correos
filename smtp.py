10428be8cb38032b44cf582319bb33b0e156065a
Andrea Perez Isla <andrea.perezisla.practicas@telefonica.com>
2019-09-13 12:23:49 +0200

    Add files via upload

--- smtp.py
+++ smtp.py
@@ -13,7 +13,7 @@ address=sys.argv[6]
 
 
 def read_template(filename):
-    with open(filename,'r', encoding='utf-8') as template_file:
+    with open(filename,'r', encoding='ISO-8859-1') as template_file:
         template_file_content = template_file.read()
     return Template(template_file_content)
 
