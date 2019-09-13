d8b7895abb1ae48e2e34c71a69110d048fac0cc1
Andrea Perez Isla <andrea.perezisla.practicas@telefonica.com>
2019-09-13 11:26:01 +0200

    Add files via upload

---
+++ smtp.py
@@ -0,0 +1,74 @@
+import boto3
+import smtplib
+import email.utils
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import jinja2
+import os
+
+import sys
+user=sys.argv[2]
+password=sys.argv[4]
+address=sys.argv[6]
+
+
+'''
+
+    Envía un email
+
+'''
+
+def send_email(user,password,address):
+
+    msg = MIMEMultipart('alternative')
+    sender = 'no-reply@na.telefonicadev.com'
+    sender_name = 'na-engineering'
+    smtp_host='email-smtp.eu-west-1.amazonaws.com'
+    smtp_port=587
+    msg['Subject'] = "correo de prueba"
+
+    msg['From'] = email.utils.formataddr((self.sender_name, self.sender))
+
+    msg['To'] = ','.join(address)
+
+    message="mensaje de prueba"
+
+    #if cc is not None:
+
+        #msg['CC'] = ','.join(cc)
+
+        #recipients = to + cc
+
+    #else:
+
+        #recipients = to
+
+
+
+    msg.attach(MIMEText(message, 'plain', 'UTF-8'))
+
+    #msg.attach(MIMEText(body_html, 'html', 'UTF-8'))
+
+
+
+    try:
+
+        server = smtplib.SMTP(smtp_host,smtp_port)
+
+        server.ehlo()
+
+        server.starttls()
+
+        server.ehlo()
+
+        server.login(user, password)
+
+        server.sendmail(sender, msg['To'], message.as_string())
+
+        server.close()
+
+        print('[INFO] Sent email.')
+
+    except Exception as e:
+
+        print("Error: ", e)
