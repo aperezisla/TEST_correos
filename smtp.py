cc0d04fb4b937174ff36165e70557d24680c9759
Andrea Perez Isla <andrea.perezisla.practicas@telefonica.com>
2019-09-13 13:00:41 +0200

    Add files via upload

--- smtp.py
+++ smtp.py
@@ -7,22 +7,13 @@ from string import Template
 import jinja2
 import os
 import sys
-user=sys.argv[2]
-password=sys.argv[4]
-address=sys.argv[6]
-
 
 def read_template(filename):
     with open(filename,'r', encoding='ISO-8859-1') as template_file:
         template_file_content = template_file.read()
     return Template(template_file_content)
 
-'''
-
-    Envía un email
-
-'''
-
+#PONER EN COPIA A NA CUANDO ESTE TODO OK
 def send_email(user,password,address):
     msg = MIMEMultipart('alternative')
     sender = 'no-reply@na.telefonicadev.com'
@@ -35,7 +26,6 @@ def send_email(user,password,address):
 
     msg['To'] =address
 
-    #message=(" Hola ......,\r\n")
     message_template = read_template('mensaje1.txt')
     message = message_template.safe_substitute(name='Nombre', entorno='int', loginurl='na-int', user_name='nombre.usuario')
     #if cc is not None:
@@ -48,13 +38,9 @@ def send_email(user,password,address):
 
         #recipients = to
 
-
-
     msg.attach(MIMEText(message, 'plain'))
 
     #msg.attach(MIMEText(body_html, 'html', 'UTF-8'))
-
-
 
     try:
 
@@ -72,10 +58,47 @@ def send_email(user,password,address):
 
         server.close()
 
-        print('[INFO] Sent email.')
+        print('Mail enviado satisfactoriamente')
 
     except Exception as e:
 
         print("Error: ", e)
 
+    #Una vez mandado el primer correo, se manda el segundo con los credenciales
+
+
+user=sys.argv[2]
+password=sys.argv[4]
+address=sys.argv[6]
+
+iam = boto3.client('iam')
+response = iam.create_user(
+    UserName='test.test'
+)
+
+#print(response)
+
+#Le añado a los dos grupos: 
+response = iam.add_user_to_group(
+    GroupName='BasicIAM',
+    UserName='test.test'
+)
+#print(response)
+
+response = iam.add_user_to_group(
+    GroupName='ForceMFA',
+    UserName='test.test'
+)
+#print(response)
+print("se ha añadido al usuario a los dos grupos")
+print("ahora se manda el primer correo")
 send_email(user,password,address)
+
+#De aqui para arriba lo hace bien, creo funcion para mandar el segundo correo
+
+print("creo la contraseña")
+response = client.create_access_key(
+    UserName='test.test',
+)
+
+print(response)
