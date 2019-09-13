adf35a05be1e2df654879398376fbd18dbe5523e
Andrea Perez Isla <andrea.perezisla.practicas@telefonica.com>
2019-09-13 12:21:32 +0200

    Add files via upload

--- smtp.py
+++ smtp.py
@@ -3,14 +3,19 @@ import smtplib
 import email.utils
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from string import Template
 import jinja2
 import os
-
 import sys
 user=sys.argv[2]
 password=sys.argv[4]
 address=sys.argv[6]
 
+
+def read_template(filename):
+    with open(filename,'r', encoding='utf-8') as template_file:
+        template_file_content = template_file.read()
+    return Template(template_file_content)
 
 '''
 
@@ -24,14 +29,15 @@ def send_email(user,password,address):
     sender_name = 'na-engineering'
     smtp_host='email-smtp.eu-west-1.amazonaws.com'
     smtp_port=587
-    msg['Subject'] = 'Hola'
+    msg['Subject'] = 'Acceso a AWS Network Analytics entorno de ....'
 
     msg['From'] = email.utils.formataddr((sender_name, sender))
 
     msg['To'] =address
 
-    message="mensaje de prueba"
-
+    #message=(" Hola ......,\r\n")
+    message_template = read_template('mensaje1.txt')
+    message = message_template.substitute(name='Nombre', entorno='int', loginurl='na-int', user_name='nombre.usuario')
     #if cc is not None:
 
         #msg['CC'] = ','.join(cc)
