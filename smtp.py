19975ad35945ef41cf07e67d4c1502b55c2a6622
Andrea Perez Isla <andrea.perezisla.practicas@telefonica.com>
2019-09-16 11:57:58 +0200

    Add files via upload

--- smtp.py
+++ smtp.py
@@ -10,6 +10,46 @@ import jinja2
 import os
 import sys
 
+#Hacer todos los casos, mejor coger el rol del usuario a crear
+stage = sys.argv[2]
+newuser = sys.argv[4]
+user=sys.argv[6]
+password=sys.argv[8]
+address=sys.argv[10]
+
+
+if stage == 'int':
+    role_arn = role_arn = 'arn:aws:iam::624472315656:role/int-na-delegated-jenkins'
+else:
+    print('Stage can only be dev, int, pro or opt')
+    sys.exit(1)
+
+sts=boto3.client('sts')
+response = sts.assume_role(
+    RoleArn=role_arn,
+    RoleSessionName='jenkins-new-user'
+)
+
+access_key_id = response['Credentials']['AccessKeyId']
+secret_access_key = response['Credentials']['SecretAccessKey']
+session_token = response['Credentials']['SessionToken']
+
+iam = boto3.client(
+    'iam',
+    aws_access_key_id=access_key_id,
+    aws_secret_access_key=secret_access_key,
+    aws_session_token=session_token,
+    region_name=region
+)
+
+# Se crea el usuario
+response = iam.create_user(
+    UserName=newuser
+)
+
+print(response)
+
+
 def read_template(filename):
     with open(filename,'r', encoding='ISO-8859-1') as template_file:
         template_file_content = template_file.read()
@@ -25,7 +65,6 @@ def send_email1(user,password,address):
     msg['Subject'] = 'Acceso a AWS Network Analytics entorno de ....'
     msg['From'] = email.utils.formataddr((sender_name, sender))
     msg['To'] =address
-    #Necesito el mensaje en html?
     message_template = read_template('mensaje1.txt')
     #poner opciones dependiendo de la cuenta donde se ha creado al usuario
     message = message_template.safe_substitute(name='Nombre', entorno='int', loginurl='na-int', user_name='nombre.usuario')
@@ -89,38 +128,13 @@ def send_email2(user,password,address):
         print('Mail enviado satisfactoriamente')
     except Exception as e:
         print("Error: ", e)
-    #Una vez mandado el primer correo, se manda el segundo con los credenciales
 
 
-user=sys.argv[2]
-password=sys.argv[4]
-address=sys.argv[6]
-
-#iam = boto3.client('iam')
-#response = iam.create_user(
-#    UserName='test.test'
-#)
-
-#print(response)
-
-#Le añado a los dos grupos: 
-#response = iam.add_user_to_group(
-#    GroupName='BasicIAM',
-#    UserName='test.test'
-#)
-#print(response)
-
-#response = iam.add_user_to_group(
-#    GroupName='ForceMFA',
-#    UserName='test.test'
-#)
-#print(response)
-#print("se ha añadido al usuario a los dos grupos")
 print("ahora se manda el primer correo")
 send_email1(user,password,address)
 
 #De aqui para arriba lo hace bien, creo funcion para mandar el segundo correo
 
 print("ahora mando el segundo correo con las credenciales")
-send_email2(user,password,address)
+#send_email2(user,password,address)
 print("se han mandado ambos correos")
