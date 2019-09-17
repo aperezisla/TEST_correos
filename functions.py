cd5e265479cd5aaeb412465e2786d7684b0b066f
Andrea Perez Isla <andrea.perezisla.practicas@telefonica.com>
2019-09-17 12:42:36 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -151,29 +151,29 @@ def coger_role(rol_user):
 
 
 def assign_role_arn(accounts,user,password,address,new_user):
-	 if accounts[0] == 1:
-	 	#Se crea cuenta en pro
-	 	pass
-	 if accounts[1] == 1:
-	 	stage = 'int'
-	 	#Se crea cuenta en int
-	 	role_arn = 'arn:aws:iam::624472315656:role/int-na-delegated-jenkins'
-	 	iam=functions.aws_connection(role_arn)
+	if accounts[0] == 1:
+		#Se crea cuenta en pro
+		pass
+	if accounts[1] == 1:
+		stage = 'int'
+		#Se crea cuenta en int
+		role_arn = 'arn:aws:iam::624472315656:role/int-na-delegated-jenkins'
+		iam=functions.aws_connection(role_arn)
 
 		# Se crea el usuario
 		response = iam.create_user(
-    		UserName=new_user
+			UserName=new_user
 		)
 
 		#Le añado a los dos grupos: 
 		response = iam.add_user_to_group(
-    		GroupName='BasicIAM',
-    		UserName=new_user
+			GroupName='BasicIAM',
+			UserName=new_user
 		)
 
 		response = iam.add_user_to_group(
-    		GroupName='ForceMFA',
-    		UserName=new_user
+			GroupName='ForceMFA',
+			UserName=new_user
 		)
 
 		create_credentials(new_user,iam)
@@ -187,10 +187,10 @@ def assign_role_arn(accounts,user,password,address,new_user):
 		print("ahora mando el segundo correo con las credenciales")
 		emails_smtp.send_email2(user,password,address,new_user,stage)
 		print("se han mandado ambos correos")
-	 	
-	 if accounts[2] == 1:
-	 	#Se crea cuenta en dev
-	 	pass
-	 if accounts[3] == 1:
-	 	#Se crea cuenta en opt
-	 	pass
+		
+	if accounts[2] == 1:
+		#Se crea cuenta en dev
+		pass
+	if accounts[3] == 1:
+		#Se crea cuenta en opt
+		pass
