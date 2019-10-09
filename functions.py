331853dee53591f85d561bbb497c587bfd728ca1
Andrea Perez Isla <andrea.perezisla.practicas@telefonica.com>
2019-10-09 11:07:21 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -243,8 +243,8 @@ def assign_role_arn(accounts,user,password,address,newuser,rol_str,mis_casos,ent
 		x.envio(msg2)
 		print('[INFO] El mail con las credenciales se ha mandado correctamente')
 		#Elimino la cuenta en la que se ha creado el usuario
-		#accounts.remove(cuenta)
-		#print('elimino la cuenta'+cuenta)
-		#print(accounts)
+		accounts.remove(cuenta)
+		print('elimino la cuenta'+cuenta)
+		print(accounts)
 
 
